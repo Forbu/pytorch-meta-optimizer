d83e4714ce60a27da85dfae688cc2df0b7a17f77
Adrien B <adri141193@yahoo.fr>
2018-02-26 13:52:40 +0000

    adding GCN LSTM

--- GraphConvLayer.py
+++ GraphConvLayer.py
@@ -146,3 +146,104 @@ class GCN_double(nn.Module):
         x = F.sigmoid(self.gc3(x, adj_1, adj_2))
         return x
 
+class GraphConvolution_LSTM(Module):
+    """
+    Simple GCN layer, similar to https://arxiv.org/abs/1609.02907
+    """
+
+    def __init__(self, in_features, out_features, bias=True):
+        super(GraphConvolution, self).__init__()
+        self.in_features = in_features
+        self.out_features = out_features
+        self.weight = Parameter(torch.Tensor(in_features + out_features, out_features))
+        if bias:
+            self.bias = Parameter(torch.Tensor(out_features))
+        else:
+            self.register_parameter('bias', None)
+            
+        # parameter LSTM 1
+        self.weight_f = Parameter(torch.Tensor(in_features + out_features, out_features))
+        
+        self.bias_f = Parameter(torch.Tensor(out_features))        
+        
+        self.weight_i = Parameter(torch.Tensor(in_features + out_features, out_features))
+
+        self.bias_i = Parameter(torch.Tensor(out_features))  
+
+        self.weight_c = Parameter(torch.Tensor(in_features + out_features, out_features))
+
+        self.bias_c = Parameter(torch.Tensor(out_features))          
+        
+        self.reset_parameters()
+        
+        # register buffer for c and h
+
+    def reset_parameters(self):
+        stdv = 1. / math.sqrt(self.weight.size(1))
+        self.weight.data.uniform_(-stdv, stdv)
+        if self.bias is not None:
+            self.bias.data.uniform_(-stdv, stdv)
+            
+        self.weight_f.data.uniform_(-stdv, stdv)
+        self.weight_i.data.uniform_(-stdv, stdv)
+        self.weight_c.data.uniform_(-stdv, stdv)
+        
+        self.bias_f.data.uniform_(-stdv, stdv)
+        self.bias_i.data.uniform_(-stdv, stdv)
+        self.bias_c.data.uniform_(-stdv, stdv)
+
+    def forward(self, input, adj, c, h):
+        """
+        Define input caract
+        sequence length - N number of node - m number of features 
+        """
+        #
+        if c == None:
+            c = self.c
+        if h == None:
+            h = self.h
+        # concat input and h
+        input_concat = torch.cat((input,h),1) 
+        
+        # LSTM equation :
+        f_i = F.sigmoid(torch.mm(self.weight_f, input_concat) + self.bias_f)
+        
+        i = F.sigmoid(torch.mm(self.weight_i, input_concat) + self.bias_i)
+        
+        c_t = F.tanh(torch.mm(self.weight_c, input_concat) + self.bias_c)
+        
+        self.c = torch.dot(c,f_i) + torch.dot(c_t,i)
+        
+        support = torch.mm(input, self.weight)
+        output = SparseMM()(adj, support)
+
+        if self.bias is not None:
+            output = output + self.bias
+
+        output = torch.dot(F.tanh(c),output)
+
+        return output, c, h
+
+    def __repr__(self):
+        return self.__class__.__name__ + ' (' \
+               + str(self.in_features) + ' -> ' \
+               + str(self.out_features) + ')'
+
+class GCN_LSTM(nn.Module):
+    def __init__(self, nfeat, nhid, n_output, dropout):
+        super(GCN_LSTM, self).__init__()
+
+        self.gc1 = GraphConvolution_LSTM(nfeat, nhid)
+        self.gc3 = GraphConvolution_LSTM(nhid, 2)
+        self.dropout = dropout
+
+    def forward(self, x, adj):
+
+        x = F.relu(self.gc1(x, adj))
+        x = F.dropout(x, self.dropout, training=self.training)
+        x = F.sigmoid(self.gc3(x, adj))
+        return x
+    
+
+
+
